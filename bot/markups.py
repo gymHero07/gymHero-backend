b0fb9bd24e7a0530d9b1056035e2dfae51764f65
Kantemir <kan.shandir94@gmail.com>
2026-02-22 16:10:38 +0300

    second commit

--- bot/markups.py
+++ bot/markups.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 launch_kb = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(
-        text="Launch ValhallaRunner",
-        web_app=WebAppInfo(url="https://runner-frontend.vercel.app/")
+        text="Launch GymHero",
+        web_app=WebAppInfo(url="https://gym-hero-frontend.vercel.app/")
     )]
 ])
